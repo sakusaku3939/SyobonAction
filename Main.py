5596107d4fcf31b891cac152aad213b8bf29929e
3939 <yuukiyuuki223@gmail.com>
2020-03-31 02:41:26 +0900

    ステージデータ.xlsxのブロック番号を全て変更

--- Main.py
+++ Main.py
@@ -57,23 +57,25 @@ def load_image():
 
 
 # ステージデータの読み込み
-def stage_load(stage_place):
-    # Excelファイルを取得
+def stage_load(sheet_name):
+    # Excelからステージデータを取得
     try:
         file = xlrd.open_workbook('res/ステージデータ.xlsx')
+        sheet = file.sheet_by_name(sheet_name)
     except FileNotFoundError:
         root = tkinter.Tk()
         root.withdraw()
-        messagebox.showerror("エラー", "ステージデータが見つかりませんでした")
-        return
+        messagebox.showerror("エラー", "ステージデータ.xlsxが見つかりませんでした")
+        pygame.quit()
+        sys.exit()
+    except xlrd.biffh.XLRDError:
+        root = tkinter.Tk()
+        root.withdraw()
+        messagebox.showerror("エラー", f"シート'{sheet_name}'が見つかりませんでした")
+        pygame.quit()
+        sys.exit()
 
-    # 番号からシート名を取得
-    for count, sheet_name in enumerate(file.sheet_names()):
-        if count == (stage_place - 1):
-            break
-
-    # シートからステージデータを取得する
-    sheet = file.sheet_by_name(str.format(sheet_name))
+    # ステージデータを読み込む
     sheet_data = [sheet.row_values(row) for row in range(16)]
     stage_data = [[int(item) for item in row if item != ''] for row in sheet_data]
 
@@ -81,58 +83,95 @@ def stage_load(stage_place):
 
 
 # ステージの描画
-def stage_draw(stage_data, scroll):
+def stage_draw(block_mode, stage_data, scroll):
+    # ステージのブロックの色
+    mode = 7 * (block_mode - 1)
+
     # 画像の読み込み
     img = load_image()
+
+    stage_position = {}
 
     # 画像の描画
     def draw(img_data, img_x, img_y, tweak_x=0, tweak_y=0):
         screen.blit(img_data, (tweak_x + img_x * 29 - scroll, tweak_y + img_y * 29 - 12))
 
-    # 敵の描画
-    # @TODO 敵の動作を追加
-    def draw_enemy(img_data, img_x, img_y, tweak_x=0, tweak_y=0):
-        screen.blit(img_data, (tweak_x + img_x * 29 - scroll, tweak_y + img_y * 29 - 12))
+    test = 0
 
     # ステージデータから取得
     for y, list_data in enumerate(stage_data):
         for x, data in enumerate(list_data):
+            stage_position[(x, y)] = data
+
             # ブロック
-            for i in range(1, 8):
+            for i in range(1, 17):
                 if data == i:
-                    draw(img[f'block{i}'], x, y)
+                    draw(img[f'block{1 + mode}'], x, y)
 
-            # 中間地点
-            if data == 30:
-                draw(img['halfway'], x, y)
+            # はてなブロック
+            for i in range(17, 29):
+                if data == i:
+                    draw(img[f'block{2 + mode}'], x, y)
 
-            # 土管
-            if data == 40:
-                draw(img['dokan1'], x, y)
-            if data == 41:
-                draw(img['dokan2'], x, y, -1, 1)
+            # 隠しブロック
+            for i in range(29, 41):
+                if data == i:
+                    draw(img[f'block{3 + mode}'], x, y)
 
-            # 山
-            if data == 80:
-                draw(img['mountain'], x, y)
+            # 硬いブロック
+            for i in range(41, 43):
+                if data == i:
+                    draw(img[f'block{4 + mode}'], x, y)
 
-            # 草
-            if data == 81:
-                draw(img['grass'], x, y)
+            # 足場ブロック
+            if data == 43:
+                # @TODO 周りのブロックを調べる処理の追加
+                if x < test:
+                    if stage_position[(x, y - 1)] != 43:
+                        draw(img[f'block{5 + mode}'], x, y)
+                    else:
+                        draw(img[f'block{6 + mode}'], x, y)
+            # if data == 44:
+            #     draw(img[f'block{6 + mode}'], x, y)
 
-            # 顔付きの雲
-            if data == 82:
-                draw(img['cloud2'], x, y)
+            # 針
+            if data == 47:
+                draw(img[f'block{7 + mode}'], x, y)
 
-            # まるい敵
-            if data == 50:
-                draw_enemy(img['enemy'], x, y)
-
-            # 甲羅亀
-            if data == 51:
-                draw_enemy(img['koura1'], x, y, 0, -12)
-
-            # @TODO c++版しょぼんのアクションのステージを全てExcelに移行
+            # 硬いブロック
+            for i in range(48, 43):
+                if data == i:
+                    draw(img[f'block{4 + mode}'], x, y)
+            # # 中間地点
+            # if data == 30:
+            #     draw(img['halfway'], x, y)
+            #
+            # # 土管
+            # if data == 40:
+            #     draw(img['dokan1'], x, y)
+            # if data == 41:
+            #     draw(img['dokan2'], x, y, -1, 1)
+            #
+            # # 山
+            # if data == 80:
+            #     draw(img['mountain'], x, y)
+            #
+            # # 草
+            # if data == 81:
+            #     draw(img['grass'], x, y)
+            #
+            # # 顔付きの雲
+            # if data == 82:
+            #     draw(img['cloud2'], x, y)
+            #
+            # # まるい敵
+            # if data == 50:
+            #     draw_enemy(img['enemy'], x, y)
+            #
+            # # 甲羅亀
+            # if data == 51:
+            #     draw_enemy(img['koura1'], x, y, 0, -12)
+            test = x
 
 
 # 黒画面にして残機を表示
@@ -239,20 +278,17 @@ class Stage_1:
         img = load_image()
         self.player = [img['player1'], img['player2'], img['player3'], img['player4'], img['player5'], img['player6']]
 
-        self.stage_data = stage_load(1)
+        self.stage_data = stage_load('1-1')
         self.init_flag = True
         self.scroll = 0
 
         remain_show()
+        screen.fill((160, 180, 250))
+        stage_draw(1, self.stage_data, self.scroll)
         self.main()
 
     def main(self):
         while 1:
-            screen.fill((160, 180, 250))
-            stage_draw(self.stage_data, self.scroll)
-
-            # @TODO プレイヤーの追加
-
             pygame.display.update()
 
             # 矢印キー入力
